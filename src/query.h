082c4020e209a1e70e9e5453265d8b449b0f34ba
Pokharel <sp5rt@rtsg2742jh97f.managed.mst.edu>
2025-06-03 13:36:46 -0500

    Removing commented code

--- src/query.h
+++ src/query.h
@@ -23,31 +23,6 @@ void BFquery(
              unsigned int numNeighbors,
              vector<pair<string, vector<pair<double, string>>>>& queryResults
              ) ;
-             
-void queryThreadFunct(
-             unordered_map<string, GEOSGeometry*> centeredQueryGeosWithID,
-             unordered_map<string, pair<GEOSGeometry*, vector<double>>> geometrySketchMap,
-             vector<HashMap<std::string>*> hashMaps,
-             int numNeighbors, 
-             int hashLength, 
-             vector<unsigned int>& seeds, 
-             int rank,
-             ResultType& LSHoutput,
-             vector<vector<GEOSGeometry*>>& global_MBR_grid,
-             const vector<bool>& cellsToKeep
-             ) ;
-                     
-void LSHHashFunction(
-             const unordered_map<string, GEOSGeometry*>& geos, // Changed to unordered_map
-             int rank,
-             const unordered_map<string, GEOSGeometry*> centeredQueryGeosWithID,
-             unsigned int numNeighbor,
-             int gridSize,
-             int hashLength ,
-            // std::vector<std::pair<std::pair<std::string, GEOSGeom_t*>, std::vector<std::pair<double, std::pair<std::string, GEOSGeom_t*>>>>>& LSHoutput,
-             ResultType& LSHoutput,
-             GEOSContextHandle_t ctx
-             ) ;
 
 void PrefixFilterFunction(
              const unordered_map<string, GEOSGeometry*>& geos, // Changed to unordered_map
